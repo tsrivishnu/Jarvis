3feae5cc991cd2c9f97b9d682efcac042a2a997b
Sri Vishnnu Totakura <srivishnu@totakura.in>
2018-08-12 17:40:36 +0200

    Add +config+ dir to maintain application config

--- app.rb
+++ app.rb
@@ -1,23 +1,29 @@
-require 'sinatra'
-require 'pry'
-require 'sinatra/config_file'
-require "sinatra/reloader" if development?
-require_relative 'lib/dialogue_flow'
+require_relative "config/application.rb"
+# require "sinatra/reloader" if development?
 
+binding.pry
+class JarvisBackend < Sinatra::Base
+  register Sinatra::ConfigFile
 
-# require_relative 'lib/google_geocode'
+  set :bind, '0.0.0.0'
 
-set :bind, '0.0.0.0'
-# config_file 'config.yml'
+  configure do
+    enable :logging # This logs only to STDOUT, not to a file
+  end
 
-get "/" do
-  content_type :json
-  "Hi"
-end
+  config_file 'config/application.yml'
 
-post "/" do
-  content_type :json
-  dialogue_flow_request = DialogueFlow::Request.new(request.body.read.to_s)
-  resp = dialogue_flow_request.intent_handler.handle
-  resp.to_json
+  get "/" do
+    content_type :json
+    "Hi"
+  end
+
+  post "/" do
+    content_type :json
+    dialogue_flow_request = DialogueFlow::Request.new(request.body.read.to_s)
+    resp = dialogue_flow_request.intent_handler.handle
+    resp.to_json
+  end
+
+  run! if app_file == $0
 end
---
+++ config/application.rb
@@ -0,0 +1,23 @@
+require 'rubygems'
+require 'bundler/setup'
+Bundler.require(:default) # This will require all the gems
+require "yaml"
+
+require_relative '../lib/dialogue_flow'
+
+class AppConfig
+  def self.load!
+    @@configuration = YAML::load_file(File.join(__dir__, 'application.yml'))
+  end
+
+  def self.method_missing(method)
+    @@configuration ||= {}
+    return @@configuration[method.to_s]
+  end
+end
+
+AppConfig.load!
+
+require_all "config" # requires all files from the directory.
+require "sinatra/config_file"
+
